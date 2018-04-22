a7f2c996051eb1f48385d2b4fb09534621f18603
Nina Satragno <nsatragno@gmail.com>
2018-04-22 16:13:51 -0300

    Better tileset.

--- app/tileset.rb
+++ app/tileset.rb
@@ -10,24 +10,27 @@ class Tileset
 
     tileset_name = @map["tilesets"][0]["image"]
     @tiles = Gosu::Image.load_tiles("maps/#{tileset_name}", 32, 32, retro: true)
-    @background = @map["layers"].find do |layer|
-      layer["name"] == "BackgroundLayer"
-    end["data"]
-    @obstacles = @map["layers"].find do |layer|
+    @layers = @map["layers"]
+    @obstacles = @layers.find do |layer|
       layer["name"] == "ObstacleLayer"
     end["data"]
+    @layers.reject! do |layer|
+      layer["name"] == "ObstacleLayer"
+    end
   end
 
   def draw
-    draw_layer @background
-    draw_layer @obstacles
+    @layers.each do |layer|
+      draw_layer layer["data"]
+    end
   end
 
   private
   def draw_layer(layer)
     layer.map do |tile_index|
-      @tiles[tile_index - 1]
+      tile_index == 0 ? nil : @tiles[tile_index - 1]
     end.each_with_index do |tile, index|
+      next unless tile
       tile.draw (index % WIDTH) * 32, (index / WIDTH) * 32, 0
     end
   end
