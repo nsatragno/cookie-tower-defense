6cce8f26d712e2c4fcb660987054d7da8f18eaeb
Nina Satragno <nsatragno@gmail.com>
2018-04-22 18:23:12 -0300

    Add cute iconbar <3

--- app/toolbar.rb
+++ app/toolbar.rb
@@ -1,14 +1,17 @@
 class Toolbar
   Y_OFFSET = 10 * 32
+  BUTTON_MARGIN = 2
+  BUTTON_SPACING = 25
   HEIGHT = 40
 
   def initialize
     @font = Gosu::Font.new 15
-    basic_turret_button = Button.new 0, Y_OFFSET, "sprites/basic_turret_icon.png" do
+    @sprite = Gosu::Image.new "sprites/iconbar.png", retro: true
+    basic_turret_button = Button.new BUTTON_MARGIN, Y_OFFSET + BUTTON_MARGIN, "sprites/basic_turret_icon.png", 24, 24 do
       Game.instance.placing_turret = BasicTurret.new
     end
 
-    cookie_shooter_button = Button.new 40, Y_OFFSET, "sprites/cookie_shooter_icon.png" do
+    cookie_shooter_button = Button.new BUTTON_MARGIN + BUTTON_SPACING, Y_OFFSET + BUTTON_MARGIN, "sprites/cookie_shooter_icon.png", 24, 24 do
       Game.instance.placing_turret = CookieShooterTurret.new
     end
 
@@ -20,6 +23,7 @@ class Toolbar
   end
 
   def draw
+    @sprite.draw 0, Y_OFFSET, 1
     @buttons.each &:draw
     @font.draw "Dough #{Game.instance.dough}", 200, Y_OFFSET + 10 / 2, 1
   end
