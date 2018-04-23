ef3cfdfa88406a3137c79641f426ca036e68e2d2
Nina Satragno <nsatragno@gmail.com>
2018-04-23 12:00:07 -0300

    Balance level 4.

--- app/enemies/apc.rb
+++ app/enemies/apc.rb
@@ -1,5 +1,5 @@
 class Apc < Enemy
   def initialize(tile_coordinates)
-    super(tile_coordinates, 100, 32, 0.25, "sprites/apc.png", 3)
+    super(tile_coordinates, 125, 32, 0.25, "sprites/apc.png", 3)
   end
 end
--- app/levels/level4.rb
+++ app/levels/level4.rb
@@ -5,31 +5,54 @@ class Level4 < Level
     @base = [17, 8]
 
     @enemies_to_spawn = [
-      { enemy: Apc, delta: 0 },
+      { enemy: BasicEnemy, delta: 60 },
+      { enemy: BasicEnemy, delta: 60 },
+      { enemy: BasicEnemy, delta: 60 },
+      { enemy: BasicEnemy, delta: 60 },
+      { enemy: BasicEnemy, delta: 60 },
+      { enemy: BasicEnemy, delta: 60 },
 
-      { enemy: FastTank, delta: 3000 },
-      { enemy: FastTank, delta: 240 },
-      { enemy: FastTank, delta: 240 },
       { enemy: FastTank, delta: 120 },
-      { enemy: FastTank, delta: 120 },
-      { enemy: FastTank, delta: 120 },
-      { enemy: FastTank, delta: 300 },
-      { enemy: FastTank, delta: 20 },
-      { enemy: FastTank, delta: 20 },
-      { enemy: FastTank, delta: 20 },
-      { enemy: FastTank, delta: 20 },
-      { enemy: FastTank, delta: 20 },
-      { enemy: FastTank, delta: 20 },
+      { enemy: FastTank, delta: 60 },
+      { enemy: FastTank, delta: 60 },
+      { enemy: FastTank, delta: 60 },
+      { enemy: FastTank, delta: 60 },
+      { enemy: FastTank, delta: 60 },
 
-      { enemy: Apc, delta: 300 },
+      { enemy: Apc, delta: 120 },
+      { enemy: FastTank, delta: 0 },
+      { enemy: FastTank, delta: 20 },
+      { enemy: Apc, delta: 60 },
+      { enemy: Apc, delta: 60 },
+      { enemy: FastTank, delta: 0 },
+      { enemy: Apc, delta: 60 },
+      { enemy: Apc, delta: 60 },
+      { enemy: FastTank, delta: 0 },
+      { enemy: Apc, delta: 60 },
+      { enemy: Apc, delta: 120 },
+      { enemy: FastTank, delta: 0 },
+      { enemy: FastTank, delta: 20 },
+      { enemy: Apc, delta: 60 },
+      { enemy: Apc, delta: 60 },
+      { enemy: FastTank, delta: 0 },
+      { enemy: Apc, delta: 60 },
+      { enemy: Apc, delta: 60 },
+      { enemy: FastTank, delta: 0 },
+      { enemy: Apc, delta: 60 },
     ]
 
-    @allowed_buttons = [0, 1]
+    @allowed_buttons = [0, 1, 2, 3, 4]
     super
   end
 
   def dough_piles
-    [DoughPile.new(5, 7, 50), DoughPile.new(6, 7, 50), DoughPile.new(7, 7, 50)]
+    [
+      DoughPile.new(5, 7, 50),
+      DoughPile.new(6, 7, 50),
+      DoughPile.new(7, 7, 50),
+      DoughPile.new(6, 2, 50),
+      DoughPile.new(6, 3, 50),
+    ]
   end
 
   def next
